e3fe37d6e25c00393a152cab67a366c7be6a5d64
Janaina <janainasoul@icloud.com>
2023-04-28 20:34:25 -0300

    mudando o background

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,13 +3,13 @@ import './time.css'
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.corSecundaria} type='color' className='input-cor' />
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria, opacity:0.6 }}>
+            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.cor} type='color' className='input-cor' />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    //console.log(<Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar ={(a)=>{}} />)
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar ={aoDeletar} />
+                    //console.log(<Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar ={(a)=>{}} />)
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar ={aoDeletar} />
                 })}
             </div>
         </section>
